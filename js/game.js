59f47c6d8112f4a98c4dedb725a7c4b15c4a0fd0
unknown <aldialvancho@gmail.com>
2021-12-14 19:54:37 +0700

    First Commit

--- js/game.js
+++ js/game.js
@@ -13,116 +13,21 @@ let world = {
     height: 60
 }
 
-let wall ={
-    width: 66,
-    height: 2
-}
-
-const texture = new THREE.TextureLoader().load( "Textures/Green_Field.jpg" );
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set( 4, 4 );
 /**
  * backgroundPlane
  */
 let backgroundPlane = new THREE.Mesh(
-    new THREE.PlaneGeometry(world.width+5, world.height+5, 1, 1),
+    new THREE.PlaneGeometry(world.width, world.height, 1, 1),
     new THREE.MeshPhongMaterial({
-        //color: 0xFFFFFF
-        map : texture
+        color: 0xFFFFFF
     })
 );
 backgroundPlane.position.z = -1;
 backgroundPlane.receiveShadow = true;
-
-//   // Draw side walls
-//   const wallShape = new THREE.Shape();
-//   wallShape.moveTo(0, 0);
-//   wallShape.lineTo(1.4, 0);
-//   wallShape.lineTo(1.4, .75);
-//   wallShape.lineTo(0, .75);
-//   wallShape.lineTo(0, 0);
-//   const wallGeometry = new THREE.ExtrudeBufferGeometry([ wallShape ], {
-//     steps: 1,
-//     depth: 5,
-//     bevelEnabled: false,
-//     curveSegments: 32
-//   });
-//   const wallA = new THREE.Mesh(wallGeometry, new THREE.MeshStandardMaterial({ color: 0xff9999 }));
-//   wallA.rotateY(-Math.PI / 2);
-//   wallA.translateZ(50);
-//   //house.add(wallA);
-  
-//   const wallB = wallA.clone();
-//   wallB.translateZ(-50);
-//   //house.add(wallB);
-
-let northernwall = new THREE.Mesh(
-    new THREE.BoxGeometry(wall.width, wall.height, 1, 1),
-    new THREE.MeshPhongMaterial({
-        color: 0xFFFFFF
-        //map : texture
-    })
-);
-northernwall.position.z = -1;
-northernwall.position.y = 32;
-northernwall.receiveShadow = true;
-
-let southernwall = new THREE.Mesh(
-    new THREE.BoxGeometry(wall.width, wall.height, 1, 1),
-    new THREE.MeshPhongMaterial({
-        color: 0xFFFFFF
-        //map : texture
-    })
-);
-southernwall.position.z = -1;
-southernwall.position.y = -32;
-southernwall.receiveShadow = true;
-
-let easternwall = new THREE.Mesh(
-    new THREE.BoxGeometry(wall.height, wall.width, 1, 1),
-    new THREE.MeshPhongMaterial({
-        color: 0xFFFFFF
-        //map : texture
-    })
-);
-easternwall.position.z = -1;
-easternwall.position.x = 32;
-easternwall.receiveShadow = true;
-
-let westernwall = new THREE.Mesh(
-    new THREE.BoxGeometry(wall.height, wall.width, 1, 1),
-    new THREE.MeshPhongMaterial({
-        color: 0xFFFFFF
-        //map : texture
-    })
-);
-westernwall.position.z = -1;
-westernwall.position.x = -32;
-westernwall.receiveShadow = true;
-// const texture = new THREE.TextureLoader().load( "Textures/Green_Field.jpg" );
-// texture.wrapS = THREE.RepeatWrapping;
-// texture.wrapT = THREE.RepeatWrapping;
-// texture.repeat.set( 4, 4 );
-scene.add(backgroundPlane,northernwall,southernwall,easternwall,westernwall);
+scene.add(backgroundPlane);
 ////    ////    ////
 ///    ////    ////
 //    ////    ////
-
-// /**
-//  * AUDIO
-//  */
-// var stream = "BGM/js_sounds_snakebgm.mp3";
-
-// var audioLoader = new THREE.AudioLoader();
-// var listener = new THREE.AudioListener();
-// var audio = new THREE.Audio(listener);
-// audioLoader.load(stream, function(buffer) {
-//     audio.setBuffer(buffer);
-//     audio.setLoop(true);
-//     audio.play();
-// });
-
 
 /**
  * ambientLight
@@ -140,10 +45,10 @@ var directionalLight = new THREE.PointLight(0xffffff, 1, 500, 0.01);
 directionalLight.castShadow = true; // default false
 
 //Set up shadow properties for the light
-// directionalLight.shadow.mapSize.width = 5120; // default 512
-// directionalLight.shadow.mapSize.height = 5120; // default 512
-// directionalLight.shadow.camera.near = 0; // default
-// directionalLight.shadow.camera.far = 1000
+directionalLight.shadow.mapSize.width = 5120; // default 512
+directionalLight.shadow.mapSize.height = 5120; // default 512
+directionalLight.shadow.camera.near = 0; // default
+directionalLight.shadow.camera.far = 1000
 // default
 scene.add(directionalLight);
 
@@ -172,43 +77,11 @@ let clock = new THREE.Clock(true);
 ///    ////    ////
 //    ////    ////
 
-/**
- * Food
- */
- let food = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshLambertMaterial({
-        color: 0xff0000
-    })
-);
-scene.add(food);
-food.castShadow = true;
-
-let foodLight = new THREE.PointLight(0xff0000, 1, 20);
-scene.add(foodLight);
-
 ////    ////    ////
 ///    ////    ////
 //    ////    ////
 function setup() {
-    spawnfood();
-}
-
-function spawnfood() {
-    food.position.x = Math.round(Math.random() * world.width - world.width / 2);
-    food.position.y = Math.round(Math.random() * world.height - world.height / 2);
-
-    foodLight.position.set(food.position.x, food.position.y, 3);
-    // var stream = "Sound_Effects/POL-cinematic-boom-01.wav";
-    // var audioLoader = new THREE.AudioLoader();
-    // var listener = new THREE.AudioListener();
-    // var audio = new THREE.Audio(listener);
-    // audioLoader.load(stream, function(buffer) {
-    //     audio.setBuffer(buffer);
-    //     audio.setLoop(true);
-    //     audio.play();
-    // });
-
+    
 }
 
 
@@ -247,42 +120,6 @@ function gameLoop(now) {
 function update() {
 
     controller.update();
-
-    if (controller.uparrow) {
-        if (player.direction == "down") {
-            player.direction = "down";
-        } else {
-            player.direction = "up";
-        }
-    }
-    if (controller.downarrow) {
-        if (player.direction == "up") {
-            player.direction = "up";
-        } else {
-            player.direction = "down";
-        }
-    }
-    if (controller.leftarrow) {
-        if (player.direction == "right") {
-            player.direction = "right";
-        } else {
-            player.direction = "left";
-        }
-    }
-    if (controller.rightarrow) {
-        if (player.direction == "left") {
-            player.direction = "left";
-        } else {
-            player.direction = "right";
-        }
-    }
-
-    if (clock.getElapsedTime() > 0.09) {
-        if (controller.keyCodes[32]) {
-        }
-        player.update();
-        clock.start();
-    }
 
 }
 ////    ////    ////
--- js/player.js
+++ js/player.js
@@ -11,21 +11,7 @@ class Player {
         this.vel = new THREE.Vector2(0, 0);
         this.acc = new THREE.Vector2(0, 0);
 
-        this.tailGeometry = new THREE.BoxGeometry(1, 1, 1);
-        this.tailMaterial = new THREE.MeshLambertMaterial({
-            color: 0xffffff
-        });
-        this.tail = [];
-
-        this.tail.push(new THREE.Mesh(this.tailGeometry, this.tailMaterial));
-        scene.add(this.tail[0]);
-
         this.snakeLen = 5;
-
-        this.invulnerable = false;
-        this.recharge = 0;
-
-        this.direction = "right"; // "up" "down" "left" "right"
 
         this.head = new THREE.Mesh(
             new THREE.BoxGeometry(1, 1, 1),
@@ -39,154 +25,11 @@ class Player {
         this.pointLight = new THREE.PointLight(0x00ff00, 1, 20);
         scene.add(this.pointLight);
 
-        var x = document.getElementById("main");
 
     }
 
     update() {
 
-        this.recharge++;
-
-        if (this.invulnerable) {
-            this.pointLight.color.setHex(0x0000ff);
-            this.head.material.color.setHex(0x0000ff);
-
-            if (this.recharge >= 15) {
-                this.invulnerable = false;
-            }
-        } else {
-            this.pointLight.color.setHex(0x00ff00);
-            this.head.material.color.setHex(0x00ff00);
-
-            if (controller.shift && this.recharge >= 30) {
-                this.invulnerable = true;
-                this.recharge = 0;
-                console.log("shift");
-            }
-        }
-
-        console.log(this.invulnerable, this.recharge);
-
-        if (this.direction == "up") {
-            this.pos.y++;
-        }
-        if (this.direction == "down") {
-            this.pos.y--;
-        }
-        if (this.direction == "left") {
-            this.pos.x--;
-        }
-        if (this.direction == "right") {
-            this.pos.x++;
-        }
-
-        if (this.pos.x > (world.width-2) / 2) {
-            console.log("dead");
-            //x.style.display = "block";
-            location.reload();
-            //document.getElementById("main").style.display = "block";
-            //this.pos.x = -world.width / 2;
-            window.location.href = "index.html";
-        }
-        if (this.pos.x < (-world.width+2) / 2) {
-            console.log("dead");
-            //x.style.display = "block";
-            location.reload();
-            //document.getElementById("backBtn").style.display = "block";
-           // this.pos.x = world.width / 2;
-           window.location.href = "index.html";
-        }
-        if (this.pos.y > (world.height-2) / 2) {
-            console.log("dead");
-            //x.style.display = "block";
-            location.reload();
-            //document.getElementById("backBtn").style.display = "block";
-            //this.pos.y = -world.height / 2;
-            window.location.href = "index.html";
-        }
-        if (this.pos.y < (-world.height+2) / 2) {
-            console.log("dead");
-            //x.style.display = "block";
-            location.reload();
-            //document.getElementById("backBtn").style.display = "block";
-            //this.pos.y = world.height / 2;
-            window.location.href = "index.html";
-        }
-
-        for (let i = 0; i <= this.tail.length - 1; i++) {
-            if (!this.invulnerable) {
-                if (this.tail[i].position.x == this.pos.x && this.tail[i].position.y == this.pos.y) {
-                    console.log("dead");
-                    location.reload();
-                }
-            }
-
-            // if (this.pos.x == world.width || this.pos.y == world.height){
-            //     console.log("dead");
-            //     location.reload();
-            // }
-
-            if (food.position.x == this.pos.x && food.position.y == this.pos.y) {
-
-                for (let i = 0; i <= 10; i++) {
-                    this.grow();
-                }
-
-                // audioLoader.load( 'Sound_Effects/POL-cinematic-boom-01.wav', function( buffer ) {
-                //     sound.setBuffer( buffer );
-                //     sound.setLoop( true );
-                //     sound.setVolume( 0.5 );
-                //     sound.play();
-                // });
-                //soundeffect()
-                spawnfood();
-                //soundeffect()
-            }
-        }
-
-        this.head.position.x = this.pos.x;
-        this.head.position.y = this.pos.y;
-
-        this.pointLight.position.set(this.pos.x, this.pos.y, 3);
-
-        for (let i = this.tail.length - 1; i >= 1; i--) {
-            let newPos = new THREE.Vector2(this.tail[i - 1].position.x, this.tail[i - 1].position.y);
-
-            this.tail[i].position.x = newPos.x;
-            this.tail[i].position.y = newPos.y;
-        }
-
-        this.tail[0].position.x = this.head.position.x;
-        this.tail[0].position.y = this.head.position.y;
-
-    }
-
-    grow() {
-
-        let cube = new THREE.Mesh(this.tailGeometry, this.tailMaterial);
-
-        cube.position.x = 100;
-        cube.position.y = 100;
-
-
-        cube.castShadow = true; //default is false
-        cube.receiveShadow = true; //default
-
-        this.tail.push(cube);
-
-        scene.add(this.tail[this.tail.length - 1]);
-    }
-
-    soundeffect(){
-        // var stream = "Sound_Effects/POL-cinematic-boom-01.wav";
-        // var audioLoader = new THREE.AudioLoader();
-        // var listener = new THREE.AudioListener();
-        // var audio = new THREE.Audio(listener);
-        // audioLoader.load(stream, function(buffer) {
-        //     audio.setBuffer(buffer);
-        //     audio.setLoop(true);
-        //     audio.play();
-        // });
     }
 
 }
