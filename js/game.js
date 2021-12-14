b69d84204aaf640fb0dfa99e0f9198761967b768
Muhammad Rafi Hayla Arifa <rafihaylaaa@gmail.com>
2021-12-14 19:31:35 +0700

    Commit Message

---
+++ js/game.js
@@ -0,0 +1,201 @@
+//          ////////////////////////////
+//         ////     SNAKE GAME     ////
+//        ////    Created By      ////
+//       ////   Jaya Jaya Team   ////
+//      ////////////////////////////
+
+//// ////
+let raycaster;
+let mouse;
+
+let world = {
+    width: 60,
+    height: 60
+}
+
+/**
+ * backgroundPlane
+ */
+let backgroundPlane = new THREE.Mesh(
+    new THREE.PlaneGeometry(world.width, world.height, 1, 1),
+    new THREE.MeshPhongMaterial({
+        color: 0xFFFFFF
+    })
+);
+backgroundPlane.position.z = -1;
+backgroundPlane.receiveShadow = true;
+scene.add(backgroundPlane);
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * ambientLight
+ */
+let ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambientLight); //
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * directionalLight 
+ */
+var directionalLight = new THREE.PointLight(0xffffff, 1, 500, 0.01);
+directionalLight.castShadow = true; // default false
+
+//Set up shadow properties for the light
+directionalLight.shadow.mapSize.width = 5120; // default 512
+directionalLight.shadow.mapSize.height = 5120; // default 512
+directionalLight.shadow.camera.near = 0; // default
+directionalLight.shadow.camera.far = 1000
+// default
+scene.add(directionalLight);
+
+/**
+ * player
+ */
+let player = new Player();
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * Game controller for all user input
+ */
+let controller = new Controller();
+controller.setup();
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * Time Keeper
+ */
+let clock = new THREE.Clock(true);
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * Food
+ */
+ let food = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshLambertMaterial({
+        color: 0xff0000
+    })
+);
+scene.add(food);
+food.castShadow = true;
+
+let foodLight = new THREE.PointLight(0xff0000, 1, 20);
+scene.add(foodLight);
+
+////    ////    ////
+///    ////    ////
+//    ////    ////
+function setup() {
+    spawnfood();
+}
+
+function spawnfood() {
+    food.position.x = Math.round(Math.random() * world.width - world.width / 2);
+    food.position.y = Math.round(Math.random() * world.height - world.height / 2);
+
+    foodLight.position.set(food.position.x, food.position.y, 3);
+
+}
+
+
+let deltaTime;
+let then = 0;
+let frameCount = 0;
+/**
+ * @description Master Function for running the game   
+ * requestAnimationFrame(gameLoop);  
+ * update();  
+ * render();  
+ */
+function gameLoop(now) {
+
+    now *= 0.001; // make it seconds
+
+    deltaTime = now - then;
+    then = now;
+
+    requestAnimationFrame(gameLoop);
+    frameCount++;
+
+    if (frameCount > 10) {
+        update();
+        render();
+    }
+
+}
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * update
+ */
+function update() {
+
+    controller.update();
+
+    if (controller.uparrow) {
+        if (player.direction == "down") {
+            player.direction = "down";
+        } else {
+            player.direction = "up";
+        }
+    }
+    if (controller.downarrow) {
+        if (player.direction == "up") {
+            player.direction = "up";
+        } else {
+            player.direction = "down";
+        }
+    }
+    if (controller.leftarrow) {
+        if (player.direction == "right") {
+            player.direction = "right";
+        } else {
+            player.direction = "left";
+        }
+    }
+    if (controller.rightarrow) {
+        if (player.direction == "left") {
+            player.direction = "left";
+        } else {
+            player.direction = "right";
+        }
+    }
+
+    if (clock.getElapsedTime() > 0.09) {
+        if (controller.keyCodes[32]) {
+        }
+        player.update();
+        clock.start();
+    }
+
+}
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * render
+ */
+function render() {
+    renderer.render(scene, camera);
+}
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+setup();
+gameLoop();
+
+////////////////////////////////////
---
+++ js/setup.js
@@ -0,0 +1,49 @@
+//          ////////////////////////////
+//         ////     SNAKE GAME     ////
+//        ////    Created By      ////
+//       ////   Jaya Jaya Team   ////
+//      ////////////////////////////
+
+//// ////
+/**
+ * Create a WebGLRenderer with shadows in the renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+renderer.shadowMap.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * This is the scene
+ */
+const scene = new THREE.Scene();
+////    ////    ////
+///    ////    ////
+//    ////    ////
+
+/**
+ * This is the camera
+ */
+const camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10000
+);
+camera.position.z = 50;
+
+// onWindowResize
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    let canvasWidth = window.innerWidth;
+    let canvasHeight = window.innerHeight;
+    renderer.setSize(canvasWidth, canvasHeight);
+    camera.aspect = canvasWidth / canvasHeight;
+    camera.updateProjectionMatrix();
+}
